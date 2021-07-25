9ceb0e3ad717111b425dc962b8efbbcb71e3cd43
Kfirinb <70212138+Kfirinb@users.noreply.github.com>
2021-07-25 20:51:54 +0300

    used here rotation 5 angle

--- Project_Img_Procss.py
+++ Project_Img_Procss.py
@@ -1,4 +1,6 @@
+import pathlib
 import numpy as np
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 from torchvision import datasets
 import math
 import sklearn.manifold as s
@@ -17,91 +19,95 @@ import os
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-# prior processing the images:
-#transform = transforms.Compose([transforms.Resize((600, 600)), transforms.Grayscale(1), transforms.ToTensor()]) #working good!!!
-transform = transforms.Compose([transforms.Grayscale(1), transforms.ToTensor()])
-root = "./archive_chest_scan_imgs"
+from tensorflow.keras import applications
 
+torch.cuda.is_available()
+
+#AUGMENTATION:
+root = "./archive_chest_scan_imgs/Data"
+datagen = ImageDataGenerator(
+        rotation_range=5,     #Random rotation between 0 and =chosen num
+        horizontal_flip=False,
+        fill_mode='reflect', cval=125)
+
+train_valid_test = [name for name in os.listdir(root) if os.path.isdir(os.path.join(root, name))]
+print(train_valid_test)
+for m in range(len(train_valid_test)):
+    sub_path = './archive_chest_scan_imgs/Data/' + train_valid_test[m]
+
+
+    sub_folders = [name for name in os.listdir(sub_path) if os.path.isdir(os.path.join(sub_path, name))]
+
+    for i in range(len(sub_folders)):
+        full_path = sub_path+'/'+sub_folders[i]
+        full_path_original = sub_path+'/'+sub_folders[i] + '/' + "ORIGINAL_" + sub_folders[i]
+        curr_len = len([name for name in os.listdir(full_path_original) if os.path.isfile(os.path.join(full_path_original,name))])
+        print(curr_len)
+        j = 0
+        save_dir = full_path + '/' + "AUGMENTED_" + sub_folders[i]
+        for batch in datagen.flow_from_directory(directory=full_path,
+                                                 batch_size=1,
+                                                 target_size=(256, 256),
+                                                 color_mode="grayscale",
+                                                 save_to_dir=save_dir,
+                                                 save_prefix='aug',
+                                                 save_format='png',
+                                                 class_mode = 'categorical'):
+            j += 1
+            if j >= curr_len:
+                break
+
+
+
+
+#Model:
+
+
+
+###~~~~~~~~~~~~~~~~~~~###
+'''
+# lets look at the filepath
+root = "./archive_chest_scan_imgs/Data/train"
+filedirectory = []
+for files in os.listdir(root):
+     filedirectory.append(os.path.join(root,files))
+#filedirectory
+
+
+
+
+
+###~~~~~~~~~~~~~~~~~~~###
+# File Directory for both the train and test
+train_path = "./archive_chest_scan_imgs/Data/train"
+test_path = "./archive_chest_scan_imgs/Data/test"
+# given that the dataset is really small, data augementation will be a good option to improve the accruracy
+train_datagen = ImageDataGenerator(rescale = 1.0/255.0,
+                                  horizontal_flip = True,
+                                  fill_mode = 'nearest',
+                                  zoom_range=0.2,
+                                  shear_range = 0.2,
+                                  width_shift_range=0.2,
+                                  height_shift_range=0.2,
+                                  rotation_range=0.4)
+train_generator = train_datagen.flow_from_directory(train_path,
+                                                   batch_size = 5,
+                                                   target_size = (350,350),
+                                                   class_mode = 'categorical')
+
+test_datagen = ImageDataGenerator(rescale = 1.0/255.0)
+test_generator = test_datagen.flow_from_directory(test_path,
+                                                   batch_size = 5,
+                                                   target_size = (350,350),
+                                                   class_mode = 'categorical')
+'''
+
+
+
+
+# prior processing the images:
+root = "./archive_chest_scan_imgs"
+# transform = transforms.Compose([transforms.Resize((600, 600)), transforms.Grayscale(1), transforms.ToTensor()]) #working good!!!
+transform = transforms.Compose([transforms.Grayscale(1), transforms.ToTensor()])
 # load image with labels - which image came from each folders
 dataset = datasets.ImageFolder(root=root, transform=transform)
-#print(len(dataset)) #get dataset length
-
-#Get max and min resolution of the images
-"""
-max_y_axis = 0
-min_y_axis = 2000
-for image in dataset:
-    if len(image[0][0][:]) > max_y_axis:
-        max_y_axis = len(image[0][0][:])
-    if len(image[0][0][:]) < min_y_axis:
-        min_y_axis = len(image[0][0][:])
-
-print("max_y_axis: ",max_y_axis)
-print("min_y_axis: ",min_y_axis)
-
-max_x_axis = 0
-min_x_axis = 2000
-for image in dataset:
-    if len(image[0][0][0]) > max_x_axis:
-        max_x_axis = len(image[0][0][0])
-    if len(image[0][0][0]) < min_x_axis:
-        min_x_axis = len(image[0][0][0])
-
-print("max_x_axis: ",max_x_axis)
-print("min_x_axis: ",min_x_axis)
-
-print("avg y axis: ",(int(max_y_axis)+int(min_y_axis))/2)
-print("avg x axis: ",(int(max_x_axis)+int(min_x_axis))/2)
-"""
-
-# plot one image for example:
-img = dataset[0][0]
-plt.imshow(img.permute(1, 2, 0))
-#print(img.shape) #get image dimensions
-
-# print(len(dataset[0][0][0][:])) #get image y axis
-# print(len(dataset[0][0][0][0])) #get image x axis
-
-
-#Augmentation part code:
-def visualize(original, augmented):
-    plt.figure()
-    plt.subplot(1,2,1)
-    plt.title('Original image')
-    plt.imshow(original)
-
-    plt.subplot(1,2,2)
-    plt.title('Augmented image')
-    plt.imshow(augmented)
-
-from PIL import Image
-img = Image.open("C:/Users/kfiri/PycharmProjects/ImageProcessingBigProject/archive_chest_scan_imgs/Data/train/adenocarcinoma_left.lower.lobe_T2_N0_M0_Ib/000005 (3).png")
-flipped = img.transpose(Image.FLIP_LEFT_RIGHT)
-
-visualize(img, flipped)
-plt.show()
-
-
-
-
-"""
-#mirror all data:
-import tensorflow_datasets as tfds
-
-def augment(image, label):
-  image = tf.cast(image, tf.float32)
-  image = tf.image.resize(image, [IMG_SIZE, IMG_SIZE])
-  image = (image / 255.0)
-  image = tf.image.random_crop(image, size=[IMG_SIZE, IMG_SIZE, 3])
-  image = tf.image.random_brightness(image, max_delta=0.5)
-  return image, label
-
-(train_ds, val_ds, test_ds), metadata = tfds.load(
-    'tf_flowers',
-     split=['train[:80%]', 'train[80%:90%]', 'train[90%:]'],
-     with_info=True,
-     as_supervised=True,)
-
-train_ds = train_ds.shuffle(1000).map(augment, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(batch_size).prefetch(AUTOTUNE)
-"""
-
